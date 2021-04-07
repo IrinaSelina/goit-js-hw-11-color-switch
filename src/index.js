e7cf7dafa6c6fb877fbe55986bc04b4d6663f148
Irina Selina <iras198308@gmail.com>
2021-04-07 23:15:57 +0300

    Добавляет цвет

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './styles.css';
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const buttonStart = document.querySelector('[data-action="start"]');
+const buttonStop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body')
+console.log(buttonStart);
+console.log(buttonStop);
+console.log(body);
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+buttonStart.addEventListener('click', changeColor)
+buttonStop.addEventListener('click', stopColor)
+let intervalId = null
+
+function changeColor() {
+    intervalId = setInterval(function () {  body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, colors.length-1)]}`;
+          buttonStart.disabled = true
+      }, 1000)
+    buttonStart.disabled = true
+}
+
+function stopColor() {
+    clearInterval(intervalId)
+    buttonStart.disabled = false
+}
+
